cdfc5d842510132d2f6d67b73169ab78189eab0e
Benedikt Otto <benedikt_o@web.de>
2020-08-09 22:07:50 +0200

    Fix argumentparser, allow for config files in build directory

--- translator/translator.py
+++ translator/translator.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
-import argparse, sys
+import sys
 import subprocess
+import configparser
+
+from pathlib import Path
 
 from linenumber_converter.converter import LineNumberConverter
 
@@ -9,20 +12,50 @@ from linenumber_converter.converter import LineNumberConverter
 #okular --unique 'test.pdf#src:24 test.tex'&
 
 
-def callOkular(pdf, line, tex):
-	cmd = 'okular --unique "%s#src:%d %s"' %(pdf, line, tex)
+def callOkular(pdf_file, line, tex_file):
+	cmd = 'okular --unique "%s#src:%d %s"' %(pdf_file, line, tex_file)
 	subprocess.call(cmd, shell=True)
 
-def main(*args, **kwarsg):
-	#TODO: fix args
-	line, source, latex, pdf = sys.argv[1:5]
-	mode = 'okular'
 
-	line = int(line)
-
-	convert = LineNumberConverter(source, latex)
+def translate(line, tex_source, tex_file, pdf_file, mode):
+	convert = LineNumberConverter(tex_source, tex_file)
 
 	newline = convert.convert(line)
 
 	if mode == 'okular':
-		callOkular(pdf, newline, latex)
+		callOkular(pdf_file, newline, tex_file)
+
+
+def load_config(texfile):
+	filename = Path(texfile).name
+	config_filename = (Path(texfile).parent / '_build' / filename).with_suffix('.conf')
+
+	config = configparser.ConfigParser()
+
+	config.read(config_filename)
+
+	return config.get('DEFAULT', 'tex_source'), config.get('DEFAULT', 'tex_file'), config.get('DEFAULT', 'pdf_file')
+
+def main(*args, **kwargs):
+	#TODO: fix args
+	if len(sys.argv) == 3 and sys.argv[2].endswith('.tex'):
+		tex_source, tex_file, pdf_file = load_config(sys.argv[2])
+		line = sys.argv[1]
+
+	elif len(sys.argv) == 5:
+		line, tex_source, tex_file, pdf_file = sys.argv[1:]
+
+	else:
+		print('Usage: %s line, tex_source, tex_file, pdf_file' %sys.argv[0])
+		print('Usage: %s line, tex_file' %sys.argv[0])
+		return
+
+	if not line.isnumeric():
+		print('Linenumber must be an integer')
+		return
+
+	line = int(line)
+
+	mode = 'okular'
+	translate(line, tex_source, tex_file, pdf_file, mode)
+
