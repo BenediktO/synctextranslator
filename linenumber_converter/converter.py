d371ef1c2733a8daad59b1c46cb13a015258cf0d
Benedikt Otto <benedikt_o@web.de>
2020-08-10 17:45:16 +0200

    Catch errors

--- linenumber_converter/converter.py
+++ linenumber_converter/converter.py
@@ -58,12 +58,15 @@ class LineNumberConverter:
         return dict
 
     def convert(self, line):
-        max_ = max(self.translater.keys())
-        min_ = min(self.translater.keys())
-        if line > max_:
-            line = max_
-        elif line < min_:
-            line = min_
+        try:
+            max_ = max(self.translater.keys())
+            min_ = min(self.translater.keys())
+            if line > max_:
+                line = max_
+            elif line < min_:
+                line = min_
+        except:
+            return None
         return self.translater.get(line, None)
 
     def convert_inv(self, line):
